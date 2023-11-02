6498fd0140d7f21bed811110af92d3cf1b2470a9
Mark Vodyanitskiy <mvodya@gmail.com>
2023-11-02 16:20:28 +1000

    Setup Sentry for kbsolver

--- kbsolver/app.py
+++ kbsolver/app.py
@@ -1,7 +1,22 @@
 import flask
 
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
+
 import solver
 
+sentry_sdk.init(
+    dsn="https://57792b43c338a4841b0266dc4962823a@dsn.frogling.com/4",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    enable_tracing=True,
+    integrations = [
+        FlaskIntegration(
+            transaction_style="url",
+        ),
+    ],
+)
 
 app = flask.Flask(__name__)
 
