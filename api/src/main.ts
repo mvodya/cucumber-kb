8d8f954f01c4900a1dedf829e271555477143703
Mark Vodyanitskiy <mvodya@gmail.com>
2023-11-02 06:00:41 +0000

    Sentry exeption handler for api

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,10 +1,21 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+import * as Sentry from '@sentry/node';
+import { SentryFilter } from './sentry.filter';
+
 async function bootstrap() {
+  Sentry.init({
+    dsn: process.env.SENTRY_DNS,
+  });
+
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new SentryFilter(httpAdapter));
+
   await app.listen(3000);
 }
 bootstrap();
